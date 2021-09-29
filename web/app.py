d064990a4085f77cb6a377fc6b644dae13a31521
luismunozg <luifer@its.com.co>
2021-09-28 23:11:27 -0500

    adujst

--- web/app.py
+++ web/app.py
@@ -19,5 +19,20 @@ def get_persons():
     response_json = json.loads(response.text)
     return response_json
 
+@app.route('/add_person')
+def add_person():
+    url = 'http://api/add_person'
+    response = requests.get(url) 
+    response_json = json.loads(response.text)
+    return response_json
+    
+@app.route('/delete_persons')
+def delete_persons():
+    url = 'http://api/delete_persons'
+    response = requests.get(url) 
+    response_json = json.loads(response.text)
+    return response_json
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
